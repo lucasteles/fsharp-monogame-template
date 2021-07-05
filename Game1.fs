88f6d4bc39fce1d835040493725da1fcd8940c70
Lucas Teles <lucas.teles@outlook.com>
2021-07-05 10:58:06 -0300

    more complete example, with movement and kinda of immutability

--- Game1.fs
+++ Game1.fs
@@ -4,15 +4,72 @@ open Microsoft.Xna.Framework
 open Microsoft.Xna.Framework.Graphics
 open Microsoft.Xna.Framework.Input
 
+
+type Transform = { Rotation: float32; Scale: float32; Position: Vector2 }
+type FSharpLogo = { Texture: Texture2D; Transform: Transform; Speed: float32 }
+
+module GameLogic =
+    let createTranform pos rot scale = {
+        Position=pos
+        Scale = scale
+        Rotation = rot
+    }
+
+    let (|KeyDown|_|) k (state: KeyboardState) =
+        if state.IsKeyDown k then Some() else None
+
+    let movementVector = function
+        | KeyDown Keys.W & KeyDown Keys.A -> Vector2(-1.f, -1.f)
+        | KeyDown Keys.W & KeyDown Keys.D -> Vector2(1.f, -1.f)
+        | KeyDown Keys.S & KeyDown Keys.A -> Vector2(-1.f, 1.f)
+        | KeyDown Keys.S & KeyDown Keys.D -> Vector2(1.f, 1.f)
+        | KeyDown Keys.W -> Vector2(0.f, -1.f)
+        | KeyDown Keys.S -> Vector2(0.f, 1.f)
+        | KeyDown Keys.A -> Vector2(-1.f, 0.f)
+        | KeyDown Keys.D -> Vector2(1.f, -0.f)
+        | _ -> Vector2.Zero
+
+    let createLogo (game: Game) =
+        let texture = game.Content.Load<_>("logo")
+
+        let position =
+            Vector2(float32 game.Window.ClientBounds.Width / 2f,
+                    float32 game.Window.ClientBounds.Height / 2f)
+
+        let transform = createTranform position 0f 1f
+        {
+            Texture =texture
+            Transform = transform
+            Speed = 100f
+        }
+
+    let updateLogo logo (time: GameTime) =
+        let { Scale=scale; Rotation=rot; Position=pos } as currentTransform = logo.Transform
+
+        let moveVector = Keyboard.GetState() |> movementVector
+        let moveOffset = moveVector * logo.Speed * (float32 time.ElapsedGameTime.TotalSeconds)
+
+        let newTransform = {
+            currentTransform with
+                Rotation = rot + 0.01f
+                Scale = if (scale < 2f) then scale + 0.05f else scale
+                Position = pos + moveOffset
+        }
+
+        { logo with Transform = newTransform }
+
+    let logoCenter logo =
+        Vector2(float32 logo.Texture.Bounds.Width, float32 logo.Texture.Bounds.Height) / 2f
+
+open GameLogic
+
 type Game1() as this =
     inherit Game()
 
     let graphics = new GraphicsDeviceManager(this)
     let mutable spriteBatch = Unchecked.defaultof<_>
 
-    let mutable logo: Texture2D = Unchecked.defaultof<_>
-    let mutable rotation = 0f
-    let mutable scale = 0f
+    let mutable logo: FSharpLogo = Unchecked.defaultof<_>
 
     do
         this.Content.RootDirectory <- "Content"
@@ -26,30 +83,30 @@ type Game1() as this =
 
     override this.LoadContent() =
         spriteBatch <- new SpriteBatch(this.GraphicsDevice)
-        logo <- this.Content.Load<_>("logo")
+        logo <- createLogo this
 
     override this.Update gameTime =
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back = ButtonState.Pressed
             || Keyboard.GetState().IsKeyDown(Keys.Escape)) then
             this.Exit()
 
-        rotation <- (rotation + 0.01f)
-
-        if (scale < 2f)
-        then scale <- scale + 0.05f
+        logo <- updateLogo logo gameTime
 
         base.Update gameTime
 
     override this.Draw gameTime =
         this.GraphicsDevice.Clear Color.LightGray
-
-        let logoCenter = Vector2(float32 logo.Bounds.Width, float32 logo.Bounds.Height) / 2f
-        let position =
-            Vector2(float32 this.Window.ClientBounds.Width / 2f,
-                    float32 this.Window.ClientBounds.Height / 2f)
-
         spriteBatch.Begin()
-        spriteBatch.Draw(logo, position, logo.Bounds, Color.White, rotation, logoCenter, scale, SpriteEffects.None, 0f)
+        spriteBatch.Draw(
+            logo.Texture,
+            logo.Transform.Position,
+            logo.Texture.Bounds,
+            Color.White,
+            logo.Transform.Rotation,
+            logoCenter logo,
+            logo.Transform.Scale,
+            SpriteEffects.None, 0f)
+
         spriteBatch.End()
 
         base.Draw(gameTime)
